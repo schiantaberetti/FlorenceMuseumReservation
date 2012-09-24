8079d4295d7d29032556df283ac8b502980c04a4
Baldo <baldo@lilik.it>
2012-09-24 09:02:49 +0200

    Add gems to Gemfile

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Museums::Application.config.secret_token = '8ee7456f7cfbd92f9fef4862e59020a1ffef14dd18096a938256b4d1c6128e2779edd11085aab93ad924b5b764e90323ff6bb01165595b4e5b61d177ff0dc148'
